f5733277f721155ee33ec842112da4b085cd5bbb
Terrence Reno Amorinov Feranza <72123566+renoferanza@users.noreply.github.com>
2020-11-27 01:03:54 +0700

    responsive

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -8,47 +8,45 @@ function Login() {
         <div className={styles.Main}>
 
             <div className={styles.doggo}>
-                <div className={styles.tinlog}>
-                    <img src="https://ik.imagekit.io/prequel/assets/TinPet_Logo_JksXcgg-ql9N.png" />
-                </div>
-                    <img className={styles.tinpetlogo} src="https://ik.imagekit.io/prequel/assets/TinPet-white_noRAcRA2FAdY.png"/>
-                <p className={styles.tagline}>Find your pet's soulmate</p>
                 <img src={dogbg} alt="login bg"></img>
             </div>
 
-            <div className={styles.Loginbox}>
-                <div>
-                    <h1>LOG IN</h1>
-                </div>
-                <div className={styles.etxt}>
-                    Email
-                    </div>
-                <div>
-                    <input type="email" className={styles.blocks}>
-                    </input>
-                </div>
-                <div className={styles.pwd}>
-                    Password
-                    </div>
-                <div>
-                    <input type="password" className={styles.blocks}></input>
-                </div>
-                <div>
-                    <a className={styles.signup} href="/register"> Sign Up?</a>
-                </div>
+            <div className={styles.Boxi}>
 
-                <NavLink to="/">
-                    <button className={styles.btn}>
-                        Login
+                <div className={styles.Loginbox}>
+                    <div>
+                        <h1>LOG IN</h1>
+                    </div>
+                    <div className={styles.etxt}>
+                        Email
+                    </div>
+                    <div>
+                        <input type="email" className={styles.blocks}>
+                        </input>
+                    </div>
+                    <div className={styles.pwd}>
+                        Password
+                    </div>
+                    <div>
+                        <input type="password" className={styles.blocks2}></input>
+                    </div>
+                    <div>
+                        <a className={styles.signup} href="/register"> Sign Up?</a>
+                    </div>
+
+                    <NavLink to="/">
+                        <button className={styles.btn}>
+                            Login
                     </button>
-                </NavLink>
+                    </NavLink>
 
-                <div className={styles.footertxt}>
-                    Get the App.
+                    <div className={styles.footertxt}>
+                        Get the App.
             </div>
-                <button className={styles.footbtn}>
-                    Play Store
+                    <button className={styles.footbtn}>
+                        Play Store
             </button>
+                </div>
             </div>
         </div>
     );
--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -7,59 +7,56 @@ function Register() {
     return (
         <div className={styles.MainRegis}>
 
-            <div className={styles.doggo}>
-                <div className={styles.tinlog}>
-                    <img src="https://ik.imagekit.io/prequel/assets/TinPet_Logo_JksXcgg-ql9N.png" />
+                <div className={styles.doggo}>
+                    <img src={dogbg} alt="login bg"></img>
                 </div>
-                <img className={styles.tinpetlogo} src="https://ik.imagekit.io/prequel/assets/TinPet-white_noRAcRA2FAdY.png" />
-                <p className={styles.tagline}>Find your pet's soulmate</p>
-                <img src={dogbg} alt="login bg"></img>
-            </div>
-            
-            <div className={styles.Regisbox}>
-                <div>
-                    <h1>SIGN UP</h1>
-                </div>
-                <div className={styles.etxt}>
-                    Email
+
+            <div className={styles.boxit}>
+                <div className={styles.Regisbox}>
+                    <div>
+                        <h1>SIGN UP</h1>
                     </div>
-                <div>
-                    <input type="email" className={styles.blocks} />
-                </div>
-
-                <div className={styles.username}>
-                    Full Name
-                </div>
-                <div>
-                    <input type="text" className={styles.blocks} />
-                </div>
-
-                <div className={styles.pwd}>
-                    Password
+                    <div className={styles.etxt}>
+                        Email
                     </div>
-                <div>
-                    <input type="password" className={styles.blocks}></input>
-                </div>
-
-                <div className={styles.pwd}>
-                    Confirm Password
+                    <div>
+                        <input type="email" className={styles.blocks} />
                     </div>
-                <div>
-                    <input type="password" className={styles.blocks} />
-                </div>
 
-                <NavLink to="/login">
-                    <button className={styles.regisbtn}>
-                        Sign Up
+                    <div className={styles.username}>
+                        Full Name
+                </div>
+                    <div>
+                        <input type="text" className={styles.blocks} />
+                    </div>
+
+                    <div className={styles.pwd}>
+                        Password
+                    </div>
+                    <div>
+                        <input type="password" className={styles.blocks}></input>
+                    </div>
+
+                    <div className={styles.pwd}>
+                        Confirm Password
+                    </div>
+                    <div>
+                        <input type="password" className={styles.blocks} />
+                    </div>
+
+                    <NavLink to="/login">
+                        <button className={styles.regisbtn}>
+                            Sign Up
                     </button>
-                </NavLink>
+                    </NavLink>
 
-                <div className={styles.footertxt}>
-                    Get the App.
+                    <div className={styles.footertxt}>
+                        Get the App.
             </div>
-                <button className={styles.footbtn}>
-                    Play Store
+                    <button className={styles.footbtn}>
+                        Play Store
             </button>
+                </div>
             </div>
         </div>
     );
